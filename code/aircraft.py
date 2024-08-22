9e482e347ae849d1294eda80e27c3e4baf5e0b7a
Owen Wiesler <owenwiesler@gmail.com>
2024-08-22 00:31:18 -0400

    Visual updates and deletion of hard code

--- code/aircraft.py
+++ code/aircraft.py
@@ -2,12 +2,12 @@ from FlightRadar24 import FlightRadar24API
 from escpos.printer import Usb
 
 fr = FlightRadar24API()
-p = Usb(0x04b8, 0x0e20, profile="TM-T88IV")
-p.profile.profile_data["media"]["width"]["pixels"] = 560
+p = Usb(0x04b8, 0x0e20)
+p.profile.profile_data["media"]["width"]["pixels"] = 560  # Number based on trial and error 
 
-center_lat = 39.95240587773885
-center_lon = -86.27362980160068
-radius = 100000 # in meters
+center_lat = # Latitude Coordinate
+center_lon = # Longitude Coordinate
+radius = # Boundary Radius
 
 # generate all flights in bounds
 bounds = fr.get_bounds_by_point(center_lat, center_lon, radius)
@@ -31,21 +31,19 @@ try:
          airline = ''
 
     # print all details
-    if airline != '':
-        p.set(align='left')
-        p.text(f'{airline}')
-        if flight.number != '':
-            p.set(align='right')
-            p.text(f'{flight.number}\n')
-
     if flight.callsign != '':
         p.set(bold=True, align='center', double_height=True, double_width=True)
         p.text(f'{flight.callsign}\n')
-        p.set(bold=False, double_height=False, double_width=False)
-   
-    if flight.registration != '':    
-         p.text(f'{flight.registration}\n')
-
+        p.set(bold=False, double_height=False, double_width=False)  
+    
+    if airline != '':
+        if flight.number != '':
+            p.set(align='center')
+            p.text(f'{airline} {flight.number}\n') 
+        else:
+            p.set(align='center')
+            p.text(f'{airline}\n')
+ 
     if flight.origin_airport_iata != '' or flight.destination_airport_iata != '':
         try:
             origin = f'{flight.origin_airport_iata}'  # {details["airport"]["origin"]["name"]}
@@ -55,14 +53,17 @@ try:
             destination = f'{flight.destination_airport_iata}'  # {details["airport"]["destination"]["name"]}
         except (TypeError):
             destination = 'None'
+        p.set(align='center')
         p.text(f'{origin} -> {destination}\n')
 
     if aircraft != '':
-         p.text(f'Aircraft: {aircraft}\n')
+         p.ln(1)
+         p.set(align='center')
+         p.text(f'{aircraft}\n')
 
-    if flight.altitude != '':
-        p.text(f'Altitude: {flight.altitude}ft\n')
-    p.text('\n')
+    if flight.registration != '':
+         p.set(align='center') 
+         p.text(f'{flight.registration}\n')
 
     # generate qr code
     link = f'flightradar24.com/{flight.callsign}'
